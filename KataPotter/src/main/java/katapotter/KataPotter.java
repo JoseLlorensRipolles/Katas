e87cb3afa0e3264fc7125371fe291c6994c62a73
Jose Llorens Ripolles <jose.llorens.ripolles@gmail.com>
2016-07-01 02:04:34 +0200

    Created a second test to chack that KataPotter calculates correctly the price of a only book.
    Changed implementation of KataPotter "calculatePrice (books)" to pass the tests

--- KataPotter/src/main/java/katapotter/KataPotter.java
+++ KataPotter/src/main/java/katapotter/KataPotter.java
@@ -4,4 +4,7 @@ package katapotter;
  * Created by josellorens on 1/07/16.
  */
 public class KataPotter {
+    public double calculePrice(int[] books) {
+        return books.length * 8;
+    }
 }
--- KataPotter/src/test/java/katapotter/KataPotterTest.java
+++ KataPotter/src/test/java/katapotter/KataPotterTest.java
@@ -23,4 +23,23 @@ public class KataPotterTest {
         double price = kataPotter.calculePrice(books);
         assertThat(price,is(0.0));
     }
+
+    @Test
+    public void return_book_price_when_one_book(){
+        int[] books = new int[]{0};
+        double price = kataPotter.calculePrice(books);
+        assertThat(price,is(8.0));
+        books = new int[]{1};
+        price = kataPotter.calculePrice(books);
+        assertThat(price,is(8.0));
+        books = new int[]{2};
+        price = kataPotter.calculePrice(books);
+        assertThat(price,is(8.0));
+        books = new int[]{3};
+        price = kataPotter.calculePrice(books);
+        assertThat(price,is(8.0));
+        books = new int[]{4};
+        price = kataPotter.calculePrice(books);
+        assertThat(price,is(8.0));
+    }
 }
